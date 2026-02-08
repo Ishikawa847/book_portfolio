ec2e1ffab99f049c71b06ab266e262cca70b623e
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2026-02-08 20:33:48 +0900

    feat: adapt routes to app.tsx

--- frontend/src/components/auth/SignUpForm.tsx
+++ frontend/src/components/auth/SignUpForm.tsx
@@ -1,16 +1,18 @@
-export default function Hero() {
+export default function SignUpForm() {
   return (
 <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-  <legend className="fieldset-legend">Page details</legend>
+  <legend className="fieldset-legend">Login</legend>
 
-  <label className="label">Title</label>
-  <input type="text" className="input" placeholder="My awesome page" />
+  <label className="label">Email</label>
+  <input type="email" className="input" placeholder="Email" />
 
-  <label className="label">Slug</label>
-  <input type="text" className="input" placeholder="my-awesome-page" />
+  <label className="label">Password</label>
+  <input type="password" className="input" placeholder="Password" />
 
-  <label className="label">Author</label>
-  <input type="text" className="input" placeholder="Name" />
+  <label className="label">Password Confirmation</label>
+  <input type="password" className="input" placeholder="Password Comfirmation" />
+
+  <button className="btn btn-neutral mt-4">Login</button>
 </fieldset>
   )
 }
