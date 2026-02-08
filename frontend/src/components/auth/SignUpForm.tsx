b386cf2c68889d0d9d52f240ff2289d1b53c2edd
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2026-02-08 20:54:59 +0900

    feat: install devise gems2

--- frontend/src/components/auth/SignUpForm.tsx
+++ frontend/src/components/auth/SignUpForm.tsx
@@ -2,7 +2,7 @@ export default function SignUpForm() {
   return (
 <div className="min-h-screen flex items-center justify-center">
 <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-  <legend className="fieldset-legend">Login</legend>
+  <legend className="fieldset-legend">Signup</legend>
 
   <label className="label">Email</label>
   <input type="email" className="input" placeholder="Email" />
