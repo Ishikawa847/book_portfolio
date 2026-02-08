173332cd6abc21944d285f29e2d01865c6c20fbb
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2026-02-08 20:33:48 +0900

    feat: adapt routes to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
 import { useState, useEffect } from 'react'
+import { Route, Routes } from "react-router-dom";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { fetchHealth } from './lib/api'
 import Landing from "./pages/Landing"
-
-
+import SignUp from "./pages/SignUp";
 
 function App() {
-  return <Landing />
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
 }
 
 export default App
