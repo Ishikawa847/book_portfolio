1af6a9152eeacbd781d89503aecca07a0e9e61d2
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2026-02-08 20:37:20 +0900

    feat: adjust layout of singupform

--- frontend/src/components/auth/SignUpForm.tsx
+++ frontend/src/components/auth/SignUpForm.tsx
@@ -1,5 +1,6 @@
 export default function SignUpForm() {
   return (
+<div className="min-h-screen flex items-center justify-center">
 <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
   <legend className="fieldset-legend">Login</legend>
 
@@ -12,7 +13,8 @@ export default function SignUpForm() {
   <label className="label">Password Confirmation</label>
   <input type="password" className="input" placeholder="Password Comfirmation" />
 
-  <button className="btn btn-neutral mt-4">Login</button>
+  <button className="btn btn-warning mt-4">Login</button>
 </fieldset>
+</div>
   )
 }
