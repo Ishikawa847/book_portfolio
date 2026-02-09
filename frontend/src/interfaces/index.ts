7e3e80d3334c7a09a896d92e97c9814cb309310b
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2026-02-10 08:50:20 +0900

    feat: make interfaces for authentication

---
+++ frontend/src/interfaces/index.ts
@@ -0,0 +1,25 @@
+// サインアップ
+export interface SignUpParams {
+  name: string
+  email: string
+  password: string
+  passwordConfirmation: string
+}
+
+// サインイン
+export interface SignInParams {
+  email: string
+  password: string
+}
+
+// ユーザー
+export interface User {
+  id: number
+  uid: string
+  provider: string
+  email: string
+  name: string
+  allowPasswordChange: boolean
+  created_at: Date
+  updated_at: Date
+}
