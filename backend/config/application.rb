0174eac5b94d0b31bd49ef9a5506d7763c5b1e07
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2026-03-19 10:14:10 +0900

    feat: change session settings in rails

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -29,6 +29,9 @@ module Myapp
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
     end
