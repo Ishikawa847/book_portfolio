5f8f72a760381baf1c3f0470d4a9f9a45a0a3152
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2026-04-17 21:15:56 +0900

    Create books table migration

---
+++ backend/db/migrate/20260417121011_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[7.2]
+  def change
+    create_table :books, id: :uuid do |t|
+      t.string :title
+      t.string :author
+      t.string :image_url
+      t.string :google_books_id
+      t.references :user, null: false, foreign_key: true, type: :uuid
+
+      t.timestamps
+    end
+  end
+end
