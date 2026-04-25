e6246e19fdc813cfd877ee7373d1bfcf4cdbf3bd
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2026-04-25 19:40:32 +0900

    feat: update users controller

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
-  skip_before_action :authenticate_api_v1_user!, only: [:show]
-
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:show]
   def show
     user = User.find(params[:id])
 
