8d59569d4e8033ff0d0b337241d4ef04fc5fa3a8
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2026-04-29 18:44:20 +0900

    feat: add avatar url in api

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -8,7 +8,19 @@ class Api::V1::UsersController < ApplicationController
       id: user.id,
       name: user.name,
       email: user.email,
-      books: user.books
+      books: user.books,
+      avatar_url: avatar_url(user)
     }
   end
+
+  private
+
+  def avatar_url(user)
+    if user.avatar.attached?
+      url_for(user.avatar)
+    else
+      "#{ENV['FRONTEND_ORIGIN']}/sample.png"
+    end
+  end
+
 end
